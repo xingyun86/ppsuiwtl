327fcea2d27a902a6b0205828d1c65aa0b0ac1e9
xingyun86 <yfpps@headquarters.com>
2020-08-07 16:53:35 +0800

    提交WTL可停靠多Tab标签的模板源代码

---
+++ src/PPSUIWTL/DockTabs/stdafx.h
@@ -0,0 +1,32 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#pragma once
+
+// Change these values to use different versions
+#define WINVER			0x0501
+#define _WIN32_WINNT	0x0501
+#define _WIN32_IE		0x0600
+#define _RICHEDIT_VER	0x0300
+
+// Set linker options
+#pragma comment(linker, "/subsystem:windows") 
+
+#include <atlbase.h>
+#include <atlcoll.h>
+#include <atlstr.h>
+
+#include <atlwin.h>
+#include <atlapp.h>
+#include <atlddx.h>
+#include <atltypes.h>
+
+extern CAppModule _Module;
+
+#include <atlsplit.h>
+#include <atlgdi.h>
+#include <atlctrls.h>
+#include <atlctrlx.h>
+#include <atlctrlw.h>
